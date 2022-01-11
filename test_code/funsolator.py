52dc5297115174c4d9b8c4de771b476652bac5b4
jacob toft pedersen <jacob@rotand.dk>
2022-01-11 22:48:34 +0100

    It passes tests
    It generates a header & cpp file in a subdirectory "tamed" this is to ensure that an IDE can pickup
    the extra definitions.

--- test_code/funsolator.py
+++ test_code/funsolator.py
@@ -1,5 +1,7 @@
 import sys
 import re
+import os
+import random
 
 from enum import Enum
 
@@ -40,15 +42,48 @@ def extract(filename, signature):
 
 
 
-def process_template(beast, template):
+def cpp_file(beast, template, name):
+    res = f'#include "{name}.h"\n'
     p = re.compile('FUNSOLATE\("(.*)"\)')
     for line in open(template, 'r'):
         m = p.match(line)
         if m:
             signature = m.group(1)
-            print(extract(beast, signature))
+            res += extract(beast, signature)
+
+    return res
+
+def include_guard(lines, name):
+    guard = name + "_" + "".join([random.choice('0123456789abcdef') for x in range(16)])
+    res = ""
+    res += f'#ifndef {guard}\n'
+    res += f'#define {guard}\n'
+    res += lines
+    res += f'#endif //{guard}\n'
+    return res
+
+def header_file(beast, template, name):
+    res = ""
+    p = re.compile('FUNSOLATE\("(.*)"\)')
+    for line in open(template, 'r'):
+        m = p.match(line)
+        if m:
+            signature = m.group(1)
+            res += signature + ";\n"
         else:
-            print(line.strip());
+            res +=line
+
+    return include_guard(res, name)
+
+def process_template(beast, template):
+    folder = os.path.join(os.path.dirname(template), "tamed")
+    name, _ = os.path.splitext(os.path.basename(template))
+    if not os.path.exists(folder):
+        os.mkdir(folder)
+    with open(os.path.join(folder, f'{name}.cpp'), 'w') as f:
+        f.write(cpp_file(beast, template, name))
+    with open(os.path.join(folder, f'{name}.h'), 'w') as f:
+        f.write(header_file(beast, template, name))
 
 
 if __name__ == "__main__":
--- test_code/test_beast.cpp
+++ test_code/test_beast.cpp
@@ -1,11 +1,11 @@
-#include <tamed_beast.h>
+#include <tamed/beast.h>
 
 int testcase() {
 
   foo bar;
 
   bar.loaded_from_weird_system = 3;
-  return intersting_fun(bar) != 4);
+  return interesting_fun(bar) != 4;
 }
 
 int main(int argc, char *argv[]) { return testcase(); }
